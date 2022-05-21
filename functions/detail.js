76c41529401073ebaa99d49e639d2db071444047
pirateTurtle <75886763+wogha95@users.noreply.github.com>
2022-05-21 12:47:04 +0900

    fix: functions 수정

--- functions/detail.js
+++ functions/detail.js
@@ -5,7 +5,10 @@ const { API_ENDPOINT, API_KEY } = process.env;
 exports.handler = async function ({ body }) {
   const options = JSON.parse(body);
   const { imdbID } = options;
-  const { data } = await axios(`${API_ENDPOINT}?apikey=${API_KEY}&i=${imdbID}`);
+  onst { data } = await axios({
+    url: `${API_ENDPOINT}?apikey=${API_KEY}&i=${imdbID}`,
+    method: 'POST'
+  });
   return  {
     statusCode: 200,
     body: JSON.stringify(data)
--- functions/movies.js
+++ functions/movies.js
@@ -5,7 +5,10 @@ const { API_ENDPOINT, API_KEY } = process.env;
 exports.handler = async function ({ body }) {
   const options = JSON.parse(body);
   const { keyword = '', page = 1 } = options;
-  const { data } = await axios(`${API_ENDPOINT}?apikey=${API_KEY}&s=${keyword}&page=${page}`);
+  const { data } = await axios({
+    url: `${API_ENDPOINT}?apikey=${API_KEY}&s=${keyword}&page=${page}`,
+    method: 'POST'
+  });
   return  {
     statusCode: 200,
     body: JSON.stringify(data)
