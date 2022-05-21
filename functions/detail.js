7b353cd9c60674686fa2de06d10952e1ab1771d2
pirateTurtle <75886763+wogha95@users.noreply.github.com>
2022-05-21 12:52:19 +0900

    fix: functions 오타 수정

--- functions/detail.js
+++ functions/detail.js
@@ -5,7 +5,7 @@ const { API_ENDPOINT, API_KEY } = process.env;
 exports.handler = async function ({ body }) {
   const options = JSON.parse(body);
   const { imdbID } = options;
-  onst { data } = await axios({
+  const { data } = await axios({
     url: `${API_ENDPOINT}?apikey=${API_KEY}&i=${imdbID}`,
     method: 'POST'
   });
